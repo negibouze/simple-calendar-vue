6f1a0d27a08d641922a181245ec7aae4347fe3cd
negibouze <itathirdchildnegi@gmail.com>
2020-02-14 00:20:29 +0900

    File addition / deletion
    ポート変更
    Calendar.vue 追加
    HelloWorld.vue 削除

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <calendar month="2020-02" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Calendar from "./components/Calendar.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Calendar
   }
 })
 export default class App extends Vue {}
---
+++ src/components/Calendar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="calendar-root">
+    <div class="day-of-week-container">
+      <span v-for="v in dayOfWeek" :key="v" class="day-of-week-item">{{
+        v
+      }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop({ type: String, required: true }) private month!: string;
+
+  dayOfWeek = ["日", "月", "火", "水", "木", "金", "土"];
+}
+</script>
+
+<style lang="scss" scoped>
+.calendar-root {
+  background-color: rgba(112, 112, 112, 0.1);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.day-of-week-container {
+  background-color: #fff;
+  display: grid;
+  grid-gap: 1px;
+  grid-template-columns: repeat(7, 1fr);
+  height: 40px;
+}
+
+.day-of-week-item {
+  align-items: center;
+  display: inline-flex;
+  justify-content: center;
+}
+</style>
