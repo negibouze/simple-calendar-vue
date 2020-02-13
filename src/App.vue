235bb19a67f19287a0da938b6dbc3643617fc354
negibouze <itathirdchildnegi@gmail.com>
2020-02-14 00:49:02 +0900

    日付間に区切り線追加
    grid-gap

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,23 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: inherit;
 }
 </style>
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="calendar-root">
-    <div class="day-of-week-container">
-      <span v-for="v in dayOfWeek" :key="v" class="day-of-week-item">{{
-        v
-      }}</span>
-    </div>
-    <div class="calendar-date-container">
-      <div v-for="v in displayPeriod" :key="v" class="calendar-date-content">
-        <span class="day">{{ v }}</span>
+  <div>
+    <div>{{ month }}</div>
+    <div class="calendar-content">
+      <div class="day-of-week-container">
+        <span v-for="v in dayOfWeek" :key="v" class="day-of-week-item">{{
+          v
+        }}</span>
+      </div>
+      <div class="calendar-date-container">
+        <div v-for="v in displayPeriod" :key="v" class="calendar-date-content">
+          <span class="day">{{ v }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -29,33 +32,36 @@ export default class HelloWorld extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.calendar-root {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
+.calendar-content {
+  background-color: rgba(112, 112, 112, 0.1);
 }
 
 .day-of-week-container {
   display: grid;
+  grid-gap: 1px;
   grid-template-columns: repeat(7, 1fr);
   height: 40px;
 }
 
 .day-of-week-item {
   align-items: center;
+  background-color: #fff;
   display: inline-flex;
   justify-content: center;
 }
 
 .calendar-date-container {
   display: grid;
+  border: solid 1px rgba(112, 112, 112, 0.1);
   flex-grow: 1;
   grid-auto-rows: minmax(74px, 1fr);
+  grid-gap: 1px;
   grid-template-columns: repeat(7, 1fr);
 }
 
 .calendar-date-content {
   align-items: center;
+  background-color: #fff;
   display: flex;
   flex-direction: column;
 }
